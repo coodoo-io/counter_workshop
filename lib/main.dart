74e0aa0b6509beb2a630117e5f8ad9e07eb3a621
Jan Marsh <Jan.Marsh@coodoo.de>
2022-09-08 15:06:40 +0200

    Cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
-import 'package:counter_workshop/src/features/counter/counter.page.dart';
+import 'package:counter_workshop/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const CounterPage(),
-    );
-  }
+  runApp(const App());
 }
