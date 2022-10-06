3d2f77c854ada4080b946c74806f127a742c7aa6
Jan Marsh <Jan.Marsh@coodoo.de>
2022-10-06 13:43:18 +0200

    Removing unused import

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,7 +5,6 @@ import 'package:counter_workshop/src/features/counter/data/datasources/remote/sr
 import 'package:counter_workshop/src/features/counter/data/repositories/counter.repository.dart';
 import 'package:counter_workshop/src/features/counter/presentation/dashboard/bloc/dashboard.bloc.dart';
 import 'package:counter_workshop/src/features/counter/presentation/dashboard/bloc/dashboard.event.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
