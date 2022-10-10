dedd1e148ec13cc349cf3a679448c53c19c6fe31
Nico Doetsch <nico.doetsch@coodoo.de>
2022-10-10 15:37:55 +0200

    Adding Firebase with Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:counter_workshop/firebase_options.dart';
 import 'package:counter_workshop/flavors.dart';
 import 'package:counter_workshop/src/app.dart';
 import 'package:counter_workshop/src/core/logger/logger.config.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:logging/logging.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart' if (dart.library.html) 'package:intl/intl_browser.dart';
+import 'package:logging/logging.dart';
 
 // Still here because Fluter on Desktop does not Support Flavors see [https://github.com/flutter/flutter/issues/64088]
 Future<void> main() async {
@@ -12,6 +14,13 @@ Future<void> main() async {
   Intl.systemLocale = await findSystemLocale(); // Tell the intl package the current system language
   setupLogger(level: Level.INFO); // Setup Logger
   F.appFlavor = Flavor.stage; // Set Flavor
+
+  //
+  // FIREBASE
+  //
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     App(),
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,17 +1,35 @@
+import 'dart:async';
+
 import 'package:counter_workshop/flavors.dart';
 import 'package:counter_workshop/src/app.dart';
 import 'package:counter_workshop/src/core/logger/logger.config.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:logging/logging.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart' if (dart.library.html) 'package:intl/intl_browser.dart';
+import 'package:logging/logging.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // makes sure plugins are initialized
-  Intl.systemLocale = await findSystemLocale(); // Tell the intl package the current system language
-  setupLogger(level: Level.INFO); // Setup Logger
-  F.appFlavor = Flavor.prod; // Set Flavor
-  runApp(
-    App(),
+  runZonedGuarded<Future<void>>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized(); // makes sure plugins are initialized
+      Intl.systemLocale = await findSystemLocale(); // Tell the intl package the current system language
+      setupLogger(level: Level.INFO); // Setup Logger
+      F.appFlavor = Flavor.prod; // Set Flavor
+
+      //
+      // FIREBASE - CRASHLYTICS
+      //
+      await Firebase.initializeApp();
+
+      // Pass all uncaught errors from the framework to Crashlytics.
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+      runApp(
+        App(),
+      );
+    },
+    (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
   );
 }
