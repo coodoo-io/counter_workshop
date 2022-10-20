405709ec44207898a6d17b9a6110becd0cdaf5bd
Nico Doetsch <nico.doetsch@coodoo.de>
2022-10-20 15:41:45 +0200

    refactoring

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -6,6 +6,7 @@ import 'package:counter_workshop/src/core/logger/logger.config.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart' if (dart.library.html) 'package:intl/intl_browser.dart';
@@ -28,7 +29,7 @@ Future<void> main() async {
       // FIREBASE - ANALYTICS
       //
       FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-      await analytics.setAnalyticsCollectionEnabled(true);
+      await analytics.setAnalyticsCollectionEnabled(kDebugMode);
 
       // Pass all uncaught errors from the framework to Crashlytics.
       FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
